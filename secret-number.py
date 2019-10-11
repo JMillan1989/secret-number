990602b34d58bf0f7c1f245b7e909f6945ad48ef
Jesus Millan <jesusmillangarcia89@gmail.com>
2019-10-11 11:59:08 +0200

    first commit

---
+++ secret-number.py
@@ -0,0 +1,7 @@
+
+secret = int(input("Guess the secret number"))
+if secret == 5:
+    print("Congratulation, you are the winner!")
+    print("The secret number is:",  secret)
+else:
+    print("Game Over!")
